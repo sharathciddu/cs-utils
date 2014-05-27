294e6ebb47fc264b07deac07ee6a21a4bd8f6f61
Sharath Ciddu <sharath@baarzo.com>
2014-05-27 13:43:52 -0700

    Script to flush dns

---
+++ cs-flushdns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+if [[ "$OSTYPE" == 'linux-gnu' ]]; then
+	service nscd restart
+	service dnsmasq restart
+	rndc restart
+	echo "Flushed DNS cache on your Linux"
+elif [[ "$OSTYPE" == 'darwin'* ]]; then
+	dscacheutil -flushcache
+	echo "Flushed DNS cache on your OSX"
+fi
